30b7a7058a25e5fe908135c0d8a244f34a3fb208
Devin Kyler Chance <stewiekidhp101@bellsouth.net>
2013-10-15 16:42:42 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songfinder::Application.config.secret_token = '9c66b7980936cc37a52540e79f22139fd0c28f0be879c99d063a4d8658618ee685e7d5cec5fae7e02d71f1e3ee08b12e0a47954c578cd0e9877f20c413bc5b3c'
